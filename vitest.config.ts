d471ff436e9532ab6dd8031831b78ab3ce4cef3f
Mikita Pilinka <mikita.pilinka@northern.tech>
2025-01-16 09:47:50 +0100

    test: added vitest with configuration
    Ticket: MEN-7855
    Changelog: None
    
    Signed-off-by: Mikita Pilinka <mikita.pilinka@northern.tech>

---
+++ vitest.config.ts
@@ -0,0 +1,34 @@
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@northern.tech/store',
+        replacement: path.resolve(__dirname, 'packages/store/src')
+      },
+      {
+        find: '@northern.tech/common-ui',
+        replacement: path.resolve(__dirname, 'packages/common-ui/src/')
+      },
+      {
+        find: '@northern.tech/helptips',
+        replacement: path.resolve(__dirname, 'packages/common-ui/src/helptips')
+      },
+      {
+        find: '@northern.tech/utils',
+        replacement: path.resolve(__dirname, 'packages/utils/src')
+      }
+    ]
+  },
+  test: {
+    //TODO: change to 3
+    retry: 0,
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './tests/setupTests.jsx'
+  }
+});
