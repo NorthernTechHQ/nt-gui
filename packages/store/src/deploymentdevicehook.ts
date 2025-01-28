4669e4e606a67a6a3009978dee314d50948ae325
Manuel Zedel <manuel.zedel@northern.tech>
2025-01-28 09:57:56 +0100

    chore: moved left over files to typescript
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- packages/store/src/deploymentdevicehook.ts
+++ packages/store/src/deploymentdevicehook.ts
@@ -12,15 +12,17 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 import { useEffect, useRef } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import { getDevicesById } from '@northern.tech/store/selectors';
 import { getDeviceById } from '@northern.tech/store/thunks';
 import { isUUID } from 'validator';
 
+import { useAppDispatch } from './store';
+
 export const useDeploymentDevice = deploymentName => {
   const isLoading = useRef(false);
-  const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
   const devicesById = useSelector(getDevicesById);
   const hasDeviceInfo = !!devicesById[deploymentName];
 
