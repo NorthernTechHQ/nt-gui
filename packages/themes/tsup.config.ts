4fa2e0b81e9a4989d70339485019ce92630b4150
Ihor Aleksandrychiev <ihor.aleksandrychiev@northern.tech>
2025-01-30 15:40:33 +0300

    chore: build styles
    Signed-off-by: Ihor Aleksandrychiev <ihor.aleksandrychiev@northern.tech>

--- packages/themes/tsup.config.ts
+++ packages/themes/tsup.config.ts
@@ -1,3 +1,13 @@
-import config from '@northern.tech/typescript-config/tsup-config.js';
+import { config as commonConfig } from '@northern.tech/typescript-config/tsup-config.js';
+import { defineConfig } from 'tsup';
+import { lessLoader } from 'esbuild-plugin-less';
 
-export default config;
+const config = {
+  ...commonConfig,
+  esbuildPlugins: [lessLoader()]
+};
+
+export default defineConfig(options => ({
+  ...config,
+  ...options
+}));
