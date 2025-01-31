9cadf3f7c3a6192933c3074ccd7d706f5f63ab0b
Manuel Zedel <manuel.zedel@northern.tech>
2025-01-31 10:30:24 +0100

    fix(store): fixed linter errors
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- packages/store/src/storehooks.ts
+++ packages/store/src/storehooks.ts
@@ -22,7 +22,7 @@ import Cookies from 'universal-cookie';
 
 import storeActions from './actions';
 import { getSessionInfo } from './auth';
-import { DEPLOYMENT_STATES, DEVICE_STATES, TIMEOUTS, onboardingSteps, timeUnits } from './constants';
+import { DEPLOYMENT_STATES, DEVICE_STATES, TIMEOUTS, timeUnits } from './constants';
 import {
   getDevicesByStatus as getDevicesByStatusSelector,
   getFeatures,
@@ -59,7 +59,7 @@ import { getComparisonCompatibleVersion, stringToBoolean } from './utils';
 const cookies = new Cookies();
 dayjs.extend(durationDayJs);
 
-const { setSnackbar, setDeviceListState, setFirstLoginAfterSignup, setTooltipsState, setShowStartupNotification } = storeActions;
+const { setDeviceListState, setFirstLoginAfterSignup, setTooltipsState, setShowStartupNotification } = storeActions;
 
 const featureFlags = [
   'hasAuditlogs',
