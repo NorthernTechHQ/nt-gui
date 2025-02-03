7b05a8d6b77220b123a80d7591b1d759fc4fcdad
Ihor Aleksandrychiev <ihor.aleksandrychiev@northern.tech>
2025-02-03 14:57:23 +0300

    fix(typescript-config): skip storybooks from building
    Signed-off-by: Ihor Aleksandrychiev <ihor.aleksandrychiev@northern.tech>

--- packages/typescript-config/tsup-config.js
+++ packages/typescript-config/tsup-config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export const config = {
   format: ['cjs', 'esm'],
   dts: true,
-  entry: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.test.{js,jsx,ts,tsx}'],
+  entry: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.test.{js,jsx,ts,tsx}', '!src/**/*.stories.{js,jsx,ts,tsx}'],
   sourcemap: true,
   skipNodeModulesBundle: true,
   clean: true,
