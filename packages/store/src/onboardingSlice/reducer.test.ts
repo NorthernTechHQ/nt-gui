673f39453a14d588268d7bd2a1599e8c3f5e4978
Mikita Pilinka <mikita.pilinka@northern.tech>
2025-04-14 11:30:14 +0200

    test: fixed types in onboardingSlice tests
    Ticket: MEN-8282
    Changelog: None
    
    Signed-off-by: Mikita Pilinka <mikita.pilinka@northern.tech>

--- packages/store/src/onboardingSlice/reducer.test.ts
+++ packages/store/src/onboardingSlice/reducer.test.ts
@@ -11,39 +11,40 @@
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
-// @ts-nocheck
+import { describe, expect, it } from 'vitest';
+
 import reducer, { actions, initialState } from '.';
 
 describe('organization reducer', () => {
   it('should return the initial state', async () => {
-    expect(reducer(undefined, {})).toEqual(initialState);
+    expect(reducer(undefined, { type: '' })).toEqual(initialState);
   });
   it('should handle setOnboardingState', async () => {
-    expect(reducer(undefined, { type: actions.setOnboardingState, payload: { foo: 'bar', showTips: true } }).showTips).toEqual(true);
-    expect(reducer(initialState, { type: actions.setOnboardingState, payload: { foo: false } }).showTips).toEqual(null);
+    expect(reducer(undefined, { type: actions.setOnboardingState.type, payload: { foo: 'bar', showTips: true } }).showTips).toEqual(true);
+    expect(reducer(initialState, { type: actions.setOnboardingState.type, payload: { foo: false } }).showTips).toEqual(null);
   });
   it('should handle SET_SHOW_ONBOARDING_HELP', async () => {
-    expect(reducer(undefined, { type: actions.setShowOnboardingHelp, payload: true }).showTips).toEqual(true);
-    expect(reducer(initialState, { type: actions.setShowOnboardingHelp, payload: false }).showTips).toEqual(false);
+    expect(reducer(undefined, { type: actions.setShowOnboardingHelp.type, payload: true }).showTips).toEqual(true);
+    expect(reducer(initialState, { type: actions.setShowOnboardingHelp.type, payload: false }).showTips).toEqual(false);
   });
   it('should handle SET_SHOW_ONBOARDING_HELP_DIALOG', async () => {
-    expect(reducer(undefined, { type: actions.setShowDismissOnboardingTipsDialog, payload: true }).showTipsDialog).toEqual(true);
-    expect(reducer(initialState, { type: actions.setShowDismissOnboardingTipsDialog, payload: false }).showTipsDialog).toEqual(false);
+    expect(reducer(undefined, { type: actions.setShowDismissOnboardingTipsDialog.type, payload: true }).showTipsDialog).toEqual(true);
+    expect(reducer(initialState, { type: actions.setShowDismissOnboardingTipsDialog.type, payload: false }).showTipsDialog).toEqual(false);
   });
   it('should handle SET_ONBOARDING_COMPLETE', async () => {
-    expect(reducer(undefined, { type: actions.setOnboardingComplete, payload: true }).complete).toEqual(true);
-    expect(reducer(initialState, { type: actions.setOnboardingComplete, payload: false }).complete).toEqual(false);
+    expect(reducer(undefined, { type: actions.setOnboardingComplete.type, payload: true }).complete).toEqual(true);
+    expect(reducer(initialState, { type: actions.setOnboardingComplete.type, payload: false }).complete).toEqual(false);
   });
   it('should handle SET_ONBOARDING_PROGRESS', async () => {
-    expect(reducer(undefined, { type: actions.setOnboardingProgress, payload: 'test' }).progress).toEqual('test');
-    expect(reducer(initialState, { type: actions.setOnboardingProgress, payload: 'test' }).progress).toEqual('test');
+    expect(reducer(undefined, { type: actions.setOnboardingProgress.type, payload: 'test' }).progress).toEqual('test');
+    expect(reducer(initialState, { type: actions.setOnboardingProgress.type, payload: 'test' }).progress).toEqual('test');
   });
   it('should handle SET_ONBOARDING_DEVICE_TYPE', async () => {
-    expect(reducer(undefined, { type: actions.setOnboardingDeviceType, payload: 'bbb' }).deviceType).toEqual('bbb');
-    expect(reducer(initialState, { type: actions.setOnboardingDeviceType, payload: 'rpi4' }).deviceType).toEqual('rpi4');
+    expect(reducer(undefined, { type: actions.setOnboardingDeviceType.type, payload: 'bbb' }).deviceType).toEqual('bbb');
+    expect(reducer(initialState, { type: actions.setOnboardingDeviceType.type, payload: 'rpi4' }).deviceType).toEqual('rpi4');
   });
   it('should handle SET_ONBOARDING_APPROACH', async () => {
-    expect(reducer(undefined, { type: actions.setOnboardingApproach, payload: 'physical' }).approach).toEqual('physical');
-    expect(reducer(initialState, { type: actions.setOnboardingApproach, payload: 'virtual' }).approach).toEqual('virtual');
+    expect(reducer(undefined, { type: actions.setOnboardingApproach.type, payload: 'physical' }).approach).toEqual('physical');
+    expect(reducer(initialState, { type: actions.setOnboardingApproach.type, payload: 'virtual' }).approach).toEqual('virtual');
   });
 });
