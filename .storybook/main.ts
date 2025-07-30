259069bc068add085a013c53a76a0eba0edc9f5d
Manuel Zedel <manuel.zedel@northern.tech>
2025-07-30 18:29:40 +0200

    chore: simplified storybook config
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,10 @@
 import type { StorybookConfig } from '@storybook/react-vite';
-import { dirname, join } from 'path';
-
-const getAbsolutePath = value => dirname(require.resolve(join(value, 'package.json')));
 
 const config: StorybookConfig = {
   stories: ['../packages/**/*.stories.tsx'],
-  addons: [getAbsolutePath('@storybook/addon-links'), getAbsolutePath('@storybook/addon-essentials')],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: {
-    name: getAbsolutePath('@storybook/react-vite'),
+    name: '@storybook/react-vite',
     options: {}
   },
   staticDirs: ['./public'],
