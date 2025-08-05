e2f5751775d301a544a12503430ab00bd8cedc58
Manuel Zedel <manuel.zedel@northern.tech>
2025-08-05 11:09:08 +0200

    chore(themes): slightly dryed up theme info access
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- packages/themes/src/MenderNext/common.ts
+++ packages/themes/src/MenderNext/common.ts
@@ -22,55 +22,57 @@ import typographyDefinitions from './figma/typography.json' with { type: 'json' 
 const typographyTokens = typographyDefinitions.collections.find(({ name }) => name === 'typography');
 const colorTokens = colorDefinitions.collections.find(({ name }) => name === 'material/colors');
 
+const themeMode = 'Mode 1';
+
 export const typography = {
-  fontFamily: typographyTokens?.variables.fontFamily.values['Mode 1'],
+  fontFamily: typographyTokens?.variables.fontFamily.values[themeMode],
   body1: {
-    fontFamily: typographyTokens?.variables['fontFamily (body)'].values['Mode 1']
+    fontFamily: typographyTokens?.variables['fontFamily (body)'].values[themeMode]
   },
   mono: {
-    fontFamily: typographyTokens?.variables['fontFamily (mono)'].values['Mode 1']
+    fontFamily: typographyTokens?.variables['fontFamily (mono)'].values[themeMode]
   }
 };
 
 export const colors = {
   grey: {
     ...grey,
-    50: colorTokens?.variables['grey (M)']['50'].values['Mode 1'] || grey[50],
-    100: colorTokens?.variables['grey (M)']['100'].values['Mode 1'] || grey[100],
-    200: colorTokens?.variables['grey (M)']['200'].values['Mode 1'] || grey[200],
-    300: colorTokens?.variables['grey (M)']['300'].values['Mode 1'] || grey[300],
-    400: colorTokens?.variables['grey (M)']['400'].values['Mode 1'] || grey[400],
-    500: colorTokens?.variables['grey (M)']['500'].values['Mode 1'] || grey[500],
-    600: colorTokens?.variables['grey (M)']['600'].values['Mode 1'] || grey[600],
-    700: colorTokens?.variables['grey (M)']['700'].values['Mode 1'] || grey[700],
-    800: colorTokens?.variables['grey (M)']['800'].values['Mode 1'] || grey[800],
-    900: colorTokens?.variables['grey (M)']['900'].values['Mode 1'] || grey[900]
+    50: colorTokens?.variables['grey (M)']['50'].values[themeMode] || grey[50],
+    100: colorTokens?.variables['grey (M)']['100'].values[themeMode] || grey[100],
+    200: colorTokens?.variables['grey (M)']['200'].values[themeMode] || grey[200],
+    300: colorTokens?.variables['grey (M)']['300'].values[themeMode] || grey[300],
+    400: colorTokens?.variables['grey (M)']['400'].values[themeMode] || grey[400],
+    500: colorTokens?.variables['grey (M)']['500'].values[themeMode] || grey[500],
+    600: colorTokens?.variables['grey (M)']['600'].values[themeMode] || grey[600],
+    700: colorTokens?.variables['grey (M)']['700'].values[themeMode] || grey[700],
+    800: colorTokens?.variables['grey (M)']['800'].values[themeMode] || grey[800],
+    900: colorTokens?.variables['grey (M)']['900'].values[themeMode] || grey[900]
   },
   purple: {
     ...blue, // ...remainder colors (A100 - A700) should be based on 'blue'
-    50: colorTokens?.variables['purple(M)']['50'].values['Mode 1'] || purple[50],
-    100: colorTokens?.variables['purple(M)']['100'].values['Mode 1'] || purple[100],
-    200: colorTokens?.variables['purple(M)']['200'].values['Mode 1'] || purple[200],
-    300: colorTokens?.variables['purple(M)']['300'].values['Mode 1'] || purple[300],
-    400: colorTokens?.variables['purple(M)']['400'].values['Mode 1'] || purple[400],
-    500: colorTokens?.variables['purple(M)']['500'].values['Mode 1'] || purple[500],
-    600: colorTokens?.variables['purple(M)']['600'].values['Mode 1'] || purple[600],
-    700: colorTokens?.variables['purple(M)']['700'].values['Mode 1'] || purple[700],
-    800: colorTokens?.variables['purple(M)']['800'].values['Mode 1'] || purple[800],
-    900: colorTokens?.variables['purple(M)']['900'].values['Mode 1'] || purple[900]
+    50: colorTokens?.variables['purple(M)']['50'].values[themeMode] || purple[50],
+    100: colorTokens?.variables['purple(M)']['100'].values[themeMode] || purple[100],
+    200: colorTokens?.variables['purple(M)']['200'].values[themeMode] || purple[200],
+    300: colorTokens?.variables['purple(M)']['300'].values[themeMode] || purple[300],
+    400: colorTokens?.variables['purple(M)']['400'].values[themeMode] || purple[400],
+    500: colorTokens?.variables['purple(M)']['500'].values[themeMode] || purple[500],
+    600: colorTokens?.variables['purple(M)']['600'].values[themeMode] || purple[600],
+    700: colorTokens?.variables['purple(M)']['700'].values[themeMode] || purple[700],
+    800: colorTokens?.variables['purple(M)']['800'].values[themeMode] || purple[800],
+    900: colorTokens?.variables['purple(M)']['900'].values[themeMode] || purple[900]
   },
   cyan: {
     ...cyan,
-    50: colorTokens?.variables['cyan (M)']['50'].values['Mode 1'] || cyan[50],
-    100: colorTokens?.variables['cyan (M)']['100'].values['Mode 1'] || cyan[100],
-    200: colorTokens?.variables['cyan (M)']['200'].values['Mode 1'] || cyan[200],
-    300: colorTokens?.variables['cyan (M)']['300'].values['Mode 1'] || cyan[300],
-    400: colorTokens?.variables['cyan (M)']['400'].values['Mode 1'] || cyan[400],
-    500: colorTokens?.variables['cyan (M)']['500'].values['Mode 1'] || cyan[500],
-    600: colorTokens?.variables['cyan (M)']['600'].values['Mode 1'] || cyan[600],
-    700: colorTokens?.variables['cyan (M)']['700'].values['Mode 1'] || cyan[700],
-    800: colorTokens?.variables['cyan (M)']['800'].values['Mode 1'] || cyan[800],
-    900: colorTokens?.variables['cyan (M)']['900'].values['Mode 1'] || cyan[900]
+    50: colorTokens?.variables['cyan (M)']['50'].values[themeMode] || cyan[50],
+    100: colorTokens?.variables['cyan (M)']['100'].values[themeMode] || cyan[100],
+    200: colorTokens?.variables['cyan (M)']['200'].values[themeMode] || cyan[200],
+    300: colorTokens?.variables['cyan (M)']['300'].values[themeMode] || cyan[300],
+    400: colorTokens?.variables['cyan (M)']['400'].values[themeMode] || cyan[400],
+    500: colorTokens?.variables['cyan (M)']['500'].values[themeMode] || cyan[500],
+    600: colorTokens?.variables['cyan (M)']['600'].values[themeMode] || cyan[600],
+    700: colorTokens?.variables['cyan (M)']['700'].values[themeMode] || cyan[700],
+    800: colorTokens?.variables['cyan (M)']['800'].values[themeMode] || cyan[800],
+    900: colorTokens?.variables['cyan (M)']['900'].values[themeMode] || cyan[900]
   }
 };
 
