aed64f9396971de30b3833429f8b3df62bf74005
Manuel Zedel <manuel.zedel@northern.tech>
2025-11-04 12:50:06 +0100

    chore(common): lint fix after deps update
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- packages/common/src/auditlogs/eventdetails/TerminalPlayer.tsx
+++ packages/common/src/auditlogs/eventdetails/TerminalPlayer.tsx
@@ -173,6 +173,11 @@ export const TerminalPlayer = ({ className, item, sessionInitialized, token }) =
 
   const { classes } = useStyles();
 
+  const resetPlayer = () => {
+    setIsPlaying(false);
+    setBufferIndex(0);
+  };
+
   useEffect(() => {
     if (!sessionInitialized) {
       return;
@@ -227,11 +232,6 @@ export const TerminalPlayer = ({ className, item, sessionInitialized, token }) =
       resetPlayer();
     }
   }, [bufferIndex, isPaused, isPlaying]);
-
-  const resetPlayer = () => {
-    setIsPlaying(false);
-    setBufferIndex(0);
-  };
 
   const onTogglePlayClick = useCallback(() => {
     if (!wasStarted) {
