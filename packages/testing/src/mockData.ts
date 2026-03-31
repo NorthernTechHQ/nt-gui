c1aaf99f7a0f23f97b2c169431034ef2a77e0d26
Manuel Zedel <manuel.zedel@northern.tech>
2026-03-31 18:22:10 +0200

    fix(testing): ensured non-reporting utils package gets used in testing package
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- packages/testing/src/mockData.ts
+++ packages/testing/src/mockData.ts
@@ -24,6 +24,7 @@ import {
 } from '@northern.tech/utils/constants';
 
 export const undefineds = /undefined|\[object Object\]/;
+
 export const menderEnvironment = {
   features: {
     hasMultitenancy: true
