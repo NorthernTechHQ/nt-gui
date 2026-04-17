0d7320a6dbfd76101bf7dbb3d02e9840213b3481
Mikita Pilinka <mikita.pilinka@northern.tech>
2026-04-17 13:15:16 +0200

    feat(themes): added code1/code2 typography variants
    Ticket: men-9289
    
    Signed-off-by: Mikita Pilinka <mikita.pilinka@northern.tech>

--- packages/themes/src/Mender/index.ts
+++ packages/themes/src/Mender/index.ts
@@ -11,6 +11,9 @@
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
+
+// to trigger types import
+export type { DummyType } from './mui';
 export { chartColorPalette } from '../Mender/common';
 export { dark } from './dark';
 export { light } from './light';
