1c708f8611e72576231ba044a3e919c1e135fc3f
Manuel Zedel <manuel.zedel@northern.tech>
2026-09-15 11:38:32 +0200

    fix(themes): let cfengine theme depend on common theme definitions
    - but rely on own definitions where they exist to reduce theme changes to a minimum
    
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- packages/themes/src/CFEngine/index.ts
+++ packages/themes/src/CFEngine/index.ts
@@ -11,5 +11,6 @@
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
+export type { DummyType } from '../common';
 export { dark } from './dark';
 export { light } from './light';
