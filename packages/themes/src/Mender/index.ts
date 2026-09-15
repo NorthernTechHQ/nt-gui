99fbbafa7eb465d8043dcbe7ea856cf991e5802f
Manuel Zedel <manuel.zedel@northern.tech>
2026-09-15 11:35:56 +0200

    chore(themes): let mender theme rely on shared theme definitions
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- packages/themes/src/Mender/index.ts
+++ packages/themes/src/Mender/index.ts
@@ -13,7 +13,7 @@
 //    limitations under the License.
 
 // to trigger types import
-export type { DummyType } from './mui';
+export type { DummyType } from '../common';
 export { chartColorPalette } from '../Mender/common';
 export { dark } from './dark';
 export { light } from './light';
